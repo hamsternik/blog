2566e7943c2a1fa64f5ef80de35dbdc9ba14462b
Nikita Khomitsevych <hamsternik9@gmail.com>
2021-12-28 16:30:55 +0200

    fix: rename posts to articles

--- Sources/Blog/Blog/Blog.swift
+++ Sources/Blog/Blog/Blog.swift
@@ -11,7 +11,7 @@ import Plot
 
 struct Blog: Website {
     enum SectionID: String, WebsiteSectionID {
-        case posts
+        case articles
         case about
     }
 
@@ -20,7 +20,7 @@ struct Blog: Website {
 
     var url = URL(string: "https://hamsterniknotes.com")!
     var name = "hamsternik notes"
-    var description = "Content and source code of my website."
+    var description = "👋 Welcome!"
     var language: Language { .english }
     var imagePath: Path? { "Resources/images/icon-black-blue-bg.png" }
     var favicon: Favicon? { .init(path: "favicon.ico", type: "image/x-icon") }
